09b23881a0136785e02a9f63269a1fa28194f12f
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-12-14 02:03:56 -0600

    imput in place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Menu from './Menu/Menu';
 import Header from './components/header/Header';
+import Search from './components/Search';
 
 function App() {
 	return (
 		<div className="App">
 			<Menu />
 			<Header />
+			<Search />
 		</div>
 	);
 }
