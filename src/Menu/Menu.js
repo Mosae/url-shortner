4ff06192c2ce22e55e93a120d3334d45323fc982
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-12-13 19:22:41 -0600

    header nav looking better

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -6,40 +6,48 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import './Menu.css';
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-	},
-	title: {
-		flexGrow: 1,
-	},
-}));
+// const useStyles = makeStyles((theme) => ({
+// 	root: {
+// 		flexGrow: 1,
+// 	},
+// 	menuButton: {
+// 		marginRight: theme.spacing(2),
+// 	},
+// 	title: {
+// 		flexGrow: 1,
+// 	},
+// }));
 
 export default function Menu() {
-	const classes = useStyles();
+	//const classes = useStyles();
 
 	return (
-		<div className={classes.root}>
+		<div className="header">
 			<Toolbar>
-				<img src={logo} alt="Shortly" />
-				{/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+				<div className="header__ul">
+					<div className="header__logo">
+						<img src={logo} alt="Shortly" />
+					</div>
+					{/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
               <MenuIcon />
             </IconButton> */}
-				<Typography variant="h6" className="menu__items">
-					Features
-				</Typography>
-				<Typography variant="h6" className="menu__items">
-					Pricing
-				</Typography>
-				<Typography variant="h6" className="menu__items">
-					Resources
-				</Typography>
-				<div className="menu__items__right">
+					<div className="header__nav">
+						<Typography variant="h6" className="menu__items">
+							Features
+						</Typography>
+						<Typography variant="h6" className="menu__items">
+							Pricing
+						</Typography>
+						<Typography variant="h6" className="menu__items">
+							Resources
+						</Typography>
+					</div>
+				</div>
+				<div className="header__login">
 					<Button color="inherit">Login</Button>
-					<Button color="inherit">Sign Up</Button>
+					<Button variant="contained" color="primary">
+						Sign Up
+					</Button>
 				</div>
 			</Toolbar>
 		</div>
