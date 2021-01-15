69186820b4a71db7f07e4d6bdad3f0dab0a4d7fe
Mosae <mosaelitsoane@gmail.com>
2021-01-14 21:29:18 -0600

    Fixed menu spacing

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import logo from '../images/logo.svg';
-import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import './Menu.css';
-
-// const useStyles = makeStyles((theme) => ({
-// 	root: {
-// 		flexGrow: 1,
-// 	},
-// 	menuButton: {
-// 		marginRight: theme.spacing(2),
-// 	},
-// 	title: {
-// 		flexGrow: 1,
-// 	},
-// }));
 
 export default function Menu() {
 	//const classes = useStyles();
