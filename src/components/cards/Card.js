c25510ac4682791d614141c09c8a8a1fc3622415
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-01-19 23:00:26 -0600

    3 cards completed

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import brandRecognition from './images/icon-brand-recognition.svg';
+import detailedRecords from './images/icon-detailed-records.svg';
+import fullyCustomizable from './images/icon-fully-customizable.svg';
 import './cards.scss';
 function Cards() {
 	return (
@@ -23,21 +25,20 @@ function Cards() {
 						</p>
 					</div>
 					<div className="statistic">
-						<img src={brandRecognition} alt="" className="statistic__icon" />
-						<h3 className="statistics__heading">Brand Recognition</h3>
+						<img src={detailedRecords} alt="" className="statistic__icon" />
+						<h3 className="statistics__heading">Detailed Records</h3>
 						<p className="statistics__subtitle ts">
-							Boost your brand recognition with each click. Generic links don't
-							mean a thing. Branded links help instil confindence in your
-							content.
+							Gain insights into who is clicking your links. Knowing when and
+							where people engage with your content helps inform better
+							decisions.
 						</p>
 					</div>
 					<div className="statistic">
-						<img src={brandRecognition} alt="" className="statistic__icon" />
-						<h3 className="statistics__heading">Brand Recognition</h3>
+						<img src={fullyCustomizable} alt="" className="statistic__icon" />
+						<h3 className="statistics__heading">Fully Customizable</h3>
 						<p className="statistics__subtitle ts">
-							Boost your brand recognition with each click. Generic links don't
-							mean a thing. Branded links help instil confindence in your
-							content.
+							Improve brand awareness and content discoverability through
+							customizable links, supercharging audience engagement.
 						</p>
 					</div>
 				</div>
