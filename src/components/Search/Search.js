1358de4e07d9e66a474ad5a32489b42d297a2ab5
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-07-26 14:38:38 -0500

    Result shows short ur;

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import shortenMobile from '../images/bg-shorten-mobile.svg';
-import shortenDesktop from '../images/bg-shorten-desktop.svg';
+import shortenMobile from '../../images/bg-shorten-mobile.svg';
+import shortenDesktop from '../../images/bg-shorten-desktop.svg';
+import { Circle, Heart } from 'react-spinners-css';
 import './_search.scss';
 function Search() {
 	//tack user input
@@ -8,18 +9,17 @@ function Search() {
 	//show links from API - array because response from api call returns multiple short URLs
 	const [links, setLinks] = useState([]);
 	const [error, setError] = useState(false);
+	const [isLoading, setLoading] = useState(false);
 	//store out API
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		<Circle size={64} color="#7f58af" />;
 		fetch(`https://api.shrtco.de/v2/shorten?url=${currentLink}`)
 			.then((res) => res.json())
 			.then((data) => {
 				if (data.ok) {
-					setLinks([
-						...links,
-						{ link: currentLink, shortLink: data.result['full_short_link2'] },
-					]);
+					setLinks([...links, { link: currentLink, shortLink: data.result['full_short_link2'] }]);
 					console.log(links);
 					setCurrentLink('');
 				} else {
@@ -39,38 +39,13 @@ function Search() {
 	return (
 		<section className="shorten">
 			<div className="search__container">
-				<form
-					action=""
-					onSubmit={handleSubmit}
-					className={error ? 'error' : ''}
-					onFocus={() => setError(false)}>
-					{/* <img
-						src={shortenMobile}
-						alt="background"
-						className="shorten-mobile-BG"
-						aria-hidden="true"
-					/> */}
-					<img
-						src={shortenDesktop}
-						alt="background"
-						className="shorten-desktop-BG"
-						aria-hidden="true"
-					/>
+				<form action="" onSubmit={handleSubmit} className={error ? 'error' : ''} onFocus={() => setError(false)}>
+					<img src={shortenDesktop} alt="background" className="shorten-desktop-BG" aria-hidden="true" />
 					<div className="shorten__input">
-						<input
-							type="text"
-							placeholder="Shorten a link here..."
-							value={currentLink}
-							onChange={handleChange}
-							aria-label="shorten-link"
-						/>
+						<input type="text" placeholder="Shorten a link here..." value={currentLink} onChange={handleChange} aria-label="shorten-link" />
 						<p className="error-message">Please add a link</p>
 					</div>
-					<input
-						type="submit"
-						className="secondary_it_button"
-						value="Shorten It!"
-					/>
+					<input type="submit" className="secondary_it_button" value="Shorten It!" />
 				</form>
 				{links && links.map((link, i) => <Result link={link} id={i} />)}
 			</div>
@@ -85,10 +60,10 @@ const Result = ({ link, i }) => {
 	}
 	return (
 		<div className="return_result" key={i}>
-			<h3 className="result__heading">{link['link']}</h3>
+			<h3 className="result__heading">{link['shortLink']}</h3>
 			<div className="result">
 				<a href={link['shortLink']} className="result__url">
-					{link['shortLink']}
+					{/* {link['shortLink']} */}
 				</a>
 				<button id="btn" className="secondary-btn" onClick={copyToClipboard}>
 					<span className="copy">Copy</span>
